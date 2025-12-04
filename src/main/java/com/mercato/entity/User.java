5fcbd9e44feedc39aab07b167b7207ed9ac47b26
Patrick-1810 <patrickprestes977@gmail.com>
2025-12-04 17:01:31 -0300

    feat: add RefreshToken entity for JWT authentication

--- src/main/java/com/mercato/entity/User.java
+++ src/main/java/com/mercato/entity/User.java
@@ -1,10 +1,15 @@
 package com.mercato.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
